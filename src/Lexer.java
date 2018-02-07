9b80bf4e2a563105280617354aa9001af31ffe5e
Brian Jahnke <brian.jahnke1@marist.edu>
2018-02-06 19:21:35 -0500

    lex almost complete. errors and quotes need to be completed

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,6 +16,7 @@ public class Lexer {
 	public static String[] comment = {"/*", "*/"};
 	public static Pattern charP = Pattern.compile("[a-z]");
 	public static Pattern digitP = Pattern.compile("[0-9]");
+	public static List<String> errorList = new ArrayList<String>();
 	//Pattern keyWordP = Pattern.compile("[a-z][a-z]+");
 	//Pattern 
 	
@@ -60,86 +62,89 @@ public class Lexer {
 		return -1;
 	}
 	
-	private static void removeComments(String input){
-		int i = 0;
-		int start = -1;
-		int end = -1;
+	private static String removeComments(String input){
+		int start = -1; 
+		int end = -1;   
+		String inputCommentRemoved = "";
 		//boolean  = true;
 		while(input.indexOf(comment[0]) != -1 && input.indexOf(comment[1]) != -1) {
 			start = input.indexOf(comment[0]);
-			end = input.indexOf(comment[1]);
-			input.repla
+				end = input.indexOf(comment[1])+2;
+			String commentStr = (String)input.subSequence(start, end);
+			inputCommentRemoved = input.replace(commentStr, "");
 		}
+		return inputCommentRemoved;
 	}
 	
-	public static List<tokenType> lex(String input) {
+	public static List<Token> lex(String input) {
 		List<Token> result = new ArrayList<Token>();
 		int quoteCounter = 0;
+		
+		input = removeComments(input);
 		for(int i = 0; i < input.length(); i++) {
+			
 			char cChar = input.charAt(i);
-			int resultIndex = matchWordList(input, keywords, i);
-			if(resultIndex > -1) {   
-				result.add(new Token(tokenType.KEYWORD, keywords[resultIndex], 0)); //need to figure out line num
+			Matcher digitM = digitP.matcher(String.valueOf(cChar));
+			Matcher charM = charP.matcher(String.valueOf(cChar));
+			
+			if(matchWordList(input, keywords, i) > -1) {   
+				int ind = matchWordList(input, keywords, i);
+				result.add(new Token(tokenType.KEYWORD, keywords[ind], 0)); //need to figure out line num
+				i += keywords[ind].length()-1;
 			}
-			else {
-				resultIndex = matchWordList(input, types, i);
-				if(resultIndex > -1){
-					result.add(new Token(tokenType.TYPE, types[resultIndex], 0));  //need to figure out line num
-				}
-				else {
-					resultIndex = matchWordList(input, boolval, i);
-					if(resultIndex > -1) {
-						result.add(new Token(tokenType.BOOLVAL, boolval[resultIndex], 0)); //need to figure out line num
-					}
-					else {
-						resultIndex = matchWordList(input, boolop, i);
-						if(resultIndex > -1) {
-							result.add(new Token(tokenType.BOOLOP, boolop[resultIndex], 0));
-						}
-					}
-				}
+			else if(matchWordList(input, types, i) > -1) {
+				int ind = matchWordList(input, boolval, i);
+				result.add(new Token(tokenType.TYPE, types[ind], 0));  //need to figure out line num
+				i += types[ind].length()-1;
+			}
+			else if(matchWordList(input, boolval, i) > -1) {
+				int ind = matchWordList(input, boolval, i);
+				result.add(new Token(tokenType.BOOLVAL, boolval[ind], 0)); //need to figure out line num
+				i += boolval[ind].length()-1;
+			}
+			else if(matchWordList(input, boolop, i) > -1) {
+				int ind = matchWordList(input, boolop, i);
+				result.add(new Token(tokenType.BOOLOP, boolop[ind], 0));
+				i += boolop[ind].length()-1;
 			}
 			if(cChar == '(') {
 				result.add(new Token(tokenType.LPAREN, "(", 0));
 			}
-			if(cChar == ')') {
+			else if(cChar == ')') {
 				result.add(new Token(tokenType.RPAREN, ")", 0));
 			}
-			if(cChar == '{') {
+			else if(cChar == '{') {
 				result.add(new Token(tokenType.LCBRACE, "{", 0));
 			}
-			if(cChar == '}') {
+			else if(cChar == '}') {
 				result.add(new Token(tokenType.RCBRACE, "}", 0));
 			}
-			if(cChar == ' ') {
+			else if(cChar == ' ') {
 				result.add(new Token(tokenType.SPACE, "\\s", 0));
 			}
-			if(cChar == '=') {
+			else if(cChar == '=') {
 				result.add(new Token(tokenType.ASSIGN, "=", 0));
 			}
-			if(cChar == '+') {
+			else if(cChar == '+') {
 				result.add(new Token(tokenType.INTOP, "+", 0));
 			}
-			if(cChar == '\"') {
+			else if(cChar == '\"') {
 				result.add(new Token(tokenType.DQUOTE, "\"", 0));
 			}
-			if(cChar == '$') {
+			else if(cChar == '$') {
 				result.add(new Token(tokenType.EOP, "$", 0));
 			}
+			else if(digitM.matches()) {
+				result.add(new Token(tokenType.DIGIT, String.valueOf(cChar), 0));
+			}
+			else if(charM.matches()) {
+				result.add(new Token(tokenType.CHAR, String.valueOf(cChar), 0));
+			}
 			else {
-				Matcher digitM = digitP.matcher(String.valueOf(cChar));
-				if(digitM.matches()) {
-					result.add(new Token(tokenType.DIGIT, String.valueOf(cChar), 0));
-				}
-				else {
-					Matcher charM = charP.matcher(String.valueOf(cChar));
-					if(charM.matches()) {
-						result.add(new Token(tokenType.CHAR, String.valueOf(cChar), 0));
-					}
-				}
+				String error = "Error: illegal Token" + cChar + "on line: " + 0; // not a real line
 			}
 		}		
-		return null;
+		return result;
 	}
 	
 	//Scans file specified in folder test, returns a charstring of entire file
@@ -158,7 +163,7 @@ public class Lexer {
 	            for(int i = 0; i < line.length(); i++) {
 	            	charString += line;
 	            }
-	            System.out.println(line);	            
+	            //System.out.println(line);	            
 			}
 			sc.close();
 		} catch (InputMismatchException | FileNotFoundException e) {
@@ -169,5 +174,9 @@ public class Lexer {
 	
 	public static void main(String[] args) {
 		String cString = ScanFileReturnCharString("test_file1.txt");
+		List<Token> tList = lex(cString);
+		System.out.println(Arrays.toString(tList.toArray()));
+		
+		
 	}
 }
