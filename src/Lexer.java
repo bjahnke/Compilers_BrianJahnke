b9724ceff3381128f4d828480f6cca8674362500
Brian Jahnke <brian.jahnke1@marist.edu>
2018-03-01 22:36:52 -0500

    Fixed up lex
    -Cleaned up lex output
    -fixed reserved words check function correctly after the 1s one is found
    -split verrifyString into 2 functions for better usability
    -fixed removeComments to check /* is before */
    -added option to run test file from default path(for me) or custom path
    (for instructor)
    -lex reporting is live instead of printing after execution

--- src/Lexer.java
+++ src/Lexer.java
@@ -19,8 +19,10 @@ public class Lexer {
 	public static List<String> errorList = new ArrayList<String>();
 	public static List<String> warningList = new ArrayList<String>();
 	public static List<String> progList = new ArrayList<String>();
-	public static int progNum = 0;   //compare with eop symbol number to see if there is a missing symbol.
+	public static int progNum = 1;   //compare with eop symbol number to see if there is a missing symbol.
 	public static int eopNum = 0;
+	public static int progErrorCount = 0;
+	public static boolean errorInProg = false;
 	//Pattern keyWordP = Pattern.compile("[a-z][a-z]+");
 	//Pattern 
 	
@@ -56,14 +58,15 @@ public class Lexer {
 			this.lineNum = lineNum;
 		}
 		@Override public String toString() {
-			return this.t + ", " + this.c + ", " + this.lineNum + "\n";
+			return this.c + " --> " + this.t + "\tln: " + this.lineNum;
 		}
 	}
 	//check reserved words as substring in charstring, returns the index of wordlist where the element matches the at
 	//index, "index", of the input String
 	private static int matchWordList(String input, String[] wordList, int index) {
 		for(int x = 0; x < wordList.length; x++){
-			if(input.indexOf(wordList[x]) == index) {
+			if(input.indexOf(wordList[x], index) == index) {
+				//System.out.println(wordList[x]);
 				return x;
 			}
 		}
@@ -73,23 +76,39 @@ public class Lexer {
 	//Verifying strings seperate from the lex method because its annoying and messy to 
 	//keep tabs on whether or not a quote is closed throughout the method.
 	//Hopefully compartmentalizing this makes it more readable despite the reuse of code.
-	private static int verrifyString(String input, int cIndex) {
-		
-		while(cIndex < input.length()) {
-			char cChar = input.charAt(cIndex);
-			Matcher charM = charP.matcher(String.valueOf(cChar));
-			if(cChar == '\"') {
-				return cIndex;
+	private static String getStringLit(String input, int cIndex){
+		int index = cIndex + 1;
+		while(index < input.length()) {
+			char cChar = input.charAt(index);
+			if(cChar == '\"' || cChar == '$') {
+				return (String)input.subSequence(cIndex, index+1);
 			}
-			else if(!charM.matches()) {
-				String error = "Syntax Error: Token \' " +cChar+ " \' is illegal or may not be present within a String type." 
-								+ "\nSolution: Remove the Token.";
-				errorList.add(error);
-				return -1;
-			}
-			cIndex++;
+			index++;
 		}
-		return cIndex;
+		return (String)input.subSequence(cIndex, index);
+	}
+	private static boolean verifyStringLit(String strLit){
+		int i = 1;
+		boolean hasNoError = true;
+		while(i < strLit.length()-1){
+			char cChar = strLit.charAt(i);
+			Matcher charM = charP.matcher(String.valueOf(cChar));
+			if(!charM.matches() && cChar != ' ') {
+				String badToken = Character.toString(cChar);
+				if(cChar == '\n'){
+					badToken = "\\n";
+				}
+				else if(cChar == '\t'){
+					badToken = "\\t";
+				}
+				System.out.println("Error: Token \'" + badToken + "\' is illegal or not allowed in string");
+				progErrorCount++;
+				errorInProg = true;
+				hasNoError = false;
+			}
+			i++;
+		}
+		return hasNoError;
 	}
 	
 	private static String removeComments(String input){
@@ -97,8 +116,10 @@ public class Lexer {
 		int end = -1;   
 		String newInput = input;
 		while(newInput.indexOf(comment[0]) != -1 && newInput.indexOf(comment[1]) != -1) {
-			start = newInput.indexOf(comment[0]);
+			if(newInput.indexOf(comment[0]) < newInput.indexOf(comment[1])){
+				start = newInput.indexOf(comment[0]);
 				end = newInput.indexOf(comment[1])+2;
+			}
 			String commentStr = (String)newInput.subSequence(start, end);
 			newInput = newInput.replace(commentStr, "");
 		}
@@ -107,92 +128,125 @@ public class Lexer {
 	
 	public static List<Token> lex(String input) {
 		List<Token> result = new ArrayList<Token>();
-		int quoteCounter = 0;
+		
+		boolean eop = false;
+		String errorMsg = "";
+		int lineNum = 1;
 		input = removeComments(input);
+		Token newTok;
+
 		for(int i = 0; i < input.length(); i++) {
-			if(i == 0){
-				progNum++;
-				//System.out.println("Program #"+progNum);
-			}
 			char cChar = input.charAt(i);
+			newTok = null;
 			Matcher digitM = digitP.matcher(String.valueOf(cChar));
 			Matcher charM = charP.matcher(String.valueOf(cChar));
-			
+			if(i == 0 || eop){
+				if(i != input.length()-1)
+				{
+					System.out.println("Lexing Program " + progNum);
+					eop = false;
+				}
+			}
 			if(matchWordList(input, keywords, i) > -1) {   
 				int ind = matchWordList(input, keywords, i);
-				result.add(new Token(tokenType.KEYWORD, keywords[ind], 0)); //need to figure out line num
+				newTok = new Token(tokenType.KEYWORD, keywords[ind], lineNum);
 				i += keywords[ind].length()-1;
 			}
 			else if(matchWordList(input, types, i) > -1) {
 				int ind = matchWordList(input, types, i);
-				result.add(new Token(tokenType.TYPE, types[ind], 0));  //need to figure out line num				
+				newTok = new Token(tokenType.TYPE, types[ind], lineNum); 
 				i += types[ind].length()-1;
 			}
 			else if(matchWordList(input, boolval, i) > -1) {
 				int ind = matchWordList(input, boolval, i);
-				result.add(new Token(tokenType.BOOLVAL, boolval[ind], 0)); //need to figure out line num
+				newTok = new Token(tokenType.BOOLVAL, boolval[ind], lineNum);
 				i += boolval[ind].length()-1;
 			}
 			else if(matchWordList(input, boolop, i) > -1) {
 				int ind = matchWordList(input, boolop, i);
-				result.add(new Token(tokenType.BOOLOP, boolop[ind], 0));
+				newTok = new Token(tokenType.BOOLOP, boolop[ind], lineNum);
 				i += boolop[ind].length()-1;
 			}
 			else if(cChar == '(') {
-				result.add(new Token(tokenType.LPAREN, "(", 0));
+				newTok = new Token(tokenType.LPAREN, "(", lineNum);
 			}
 			else if(cChar == ')') {
-				result.add(new Token(tokenType.RPAREN, ")", 0));
+				newTok = new Token(tokenType.RPAREN, ")", lineNum);
 			}
 			else if(cChar == '{') {
-				result.add(new Token(tokenType.LCBRACE, "{", 0));
+				newTok = new Token(tokenType.LCBRACE, "{", lineNum);
 			}
 			else if(cChar == '}') {
-				result.add(new Token(tokenType.RCBRACE, "}", 0));
+				newTok = new Token(tokenType.RCBRACE, "}", lineNum);
 			}
 			else if(cChar == ' ') {
-				result.add(new Token(tokenType.SPACE, "\\s", 0));
+				newTok = new Token(tokenType.SPACE, "\\s", lineNum);
 			}
 			else if(cChar == '=') {
-				result.add(new Token(tokenType.ASSIGN, "=", 0));
+				newTok = new Token(tokenType.ASSIGN, "=", lineNum);
 			}
 			else if(cChar == '+') {
-				result.add(new Token(tokenType.INTOP, "+", 0));
+				newTok = new Token(tokenType.INTOP, "+", lineNum);
 			}
 			else if(cChar == '\"') {
-				int iOfEndQuote = verrifyString(input, i);
-				if(iOfEndQuote != -1) {
-					if(iOfEndQuote == input.length() && input.charAt(iOfEndQuote) == '$') {
-						String error = "Syntax Error: String is initiated at character num " + i + "but does not close" + 
-										"\nSolution: Add a quote to close the String or remove the existing quote";
-						errorList.add(error);
+				String strLit = getStringLit(input, i);
+				//System.out.println(strLit);
+				if(strLit.charAt(strLit.length()-1) == '\"'){
+					i = i + strLit.length();
+					if(verifyStringLit(strLit)){
+						newTok = new Token(tokenType.STRINGLITERAL, strLit, lineNum);
 					}
-					else {
-						String validString = (String)input.subSequence(i, iOfEndQuote);
-						result.add(new Token(tokenType.STRINGLITERAL, validString, 0));
-					}
+				}
+				else{
+					errorMsg = "Error: Unterminated string";
+					errorInProg = true;
+					progErrorCount++;
+					errorList.add(errorMsg);
+					i = i + strLit.length()-2;    //-1 to not skip over eop, maybe matters
 				}
 			}
 			else if(cChar == '$') {
-				result.add(new Token(tokenType.EOP, "$", 0));
-				progList.add((String)input.subSequence(0, i));
-				eopNum++;
+				newTok = new Token(tokenType.EOP, "$", lineNum);
+				//progList.add((String)input.subSequence(0, i));
+				//eopNum++;
 				if(i < input.length()-1) {
 					progNum++;
 				}
+				eop = true;
 			}
 			else if(digitM.matches()) {
-				result.add(new Token(tokenType.DIGIT, String.valueOf(cChar), 0));
+				newTok = new Token(tokenType.DIGIT, String.valueOf(cChar), lineNum);
 			}
 			else if(charM.matches()) {
-				result.add(new Token(tokenType.CHAR, String.valueOf(cChar), 0));
+				newTok = new Token(tokenType.ID, String.valueOf(cChar), lineNum);
 			}
-			else if(String.valueOf(cChar) != "\t") {
-				if(String.valueOf(cChar) != "\n") {
-					String error = "Error: illegal Token \'" + cChar + "\'on line: " + 0 + 
-						        	"\nSolution: Remove Token.\n"; // not a real line
-					errorList.add(error);
+			else if(cChar == '\n') {
+				lineNum++;
+			}
+			else if(cChar != '\t') {
+					errorMsg = "Error: Illegal Token \'" + cChar + "\', ln: " + lineNum;
+					errorInProg = true;
+					progErrorCount++;
+					errorList.add(errorMsg);
+			}
+			
+			if(newTok != null){
+				result.add(newTok);
+				System.out.println(newTok.toString());
+				if(eop){
+					if(errorInProg){
+						System.out.println("Lex completed with " + progErrorCount + " error(s)\n");
+						errorInProg = false;
+						progErrorCount = 0;
+					}
+					else{
+						System.out.println("Lex completed successfully\n");
+					}
 				}
+			}
+			else if(errorMsg != ""){
+				System.out.println(errorMsg);
+				errorMsg = "";
 			}
 		}	
 		if(input.charAt(input.length()-1) != '$') {
@@ -201,22 +255,38 @@ public class Lexer {
 		}
 		return result;
 	}
+/*-----------------|
+ *                 |
+ * Lex Report      |
+ *                 |
+ -----------------*/
+	public static void printLexReport(List<Token> tokenList){
+		for(int i = 0; i < tokenList.size(); i++){
+			String tokenObjInfo = tokenList.get(i).toString();
+			System.out.println(tokenObjInfo);
+		}
+			
+	}
 	
-	//Scans file specified in folder test, returns a charstring of entire file
-	public static String ScanFileReturnCharString(String fileName) {
+	
+/*------------------------|
+ *                        |
+ * Test File Load Methods |
+ *                        |
+ ------------------------*/
+	public static String ScanFileReturnCharString(File testFile) {
 		String charString = "";
-		File codeFile = new File(fileName); //scans the specified file at the given directory
 		try {
-			Scanner sc = new Scanner(codeFile);
+			Scanner sc = new Scanner(testFile);
 			
 			String line = "";
 			while (sc.hasNextLine()) {
 	            line = sc.nextLine();
 	            line = line.trim();
+	            line = line + "\n";
 	            for(int i = 0; i < line.length(); i++) {
 	            	charString += line.charAt(i);
-	            }
-	            //System.out.println(line);	            
+	            }	            
 			}
 			sc.close();
 		} catch (InputMismatchException | FileNotFoundException e) {
@@ -225,13 +295,68 @@ public class Lexer {
 		return charString;
 	}
 	
+	public static boolean customPathExists(File customFile){
+		boolean exists = true;
+		try{
+			Scanner fileReader = new Scanner(customFile);
+			fileReader.close();
+		}
+		catch (FileNotFoundException e){
+			System.out.println("Error: File not found at specified path \n"
+					+ "");
+			exists = false;
+		}
+		return exists;
+	}
+	
+	public static File getTestFile(){
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Options:\n"
+				+ "(1) Test file with custom path.\n"
+				+ "(2) Default test file.");
+		while(true){
+			System.out.println("Chose 1 or 2: ");
+			int option = reader.nextInt();
+			if(option == 1){
+				System.out.println("Test File Path (include file name): ");
+				String testFilePath = reader.next();
+				File customFile = new File(testFilePath);
+				boolean pathExists = customPathExists(customFile);
+				if(pathExists){
+					reader.close();
+					return customFile;
+				}
+			}
+			else if(option == 2){
+				reader.close();
+				File codeFile = new File("test_file1.txt");
+				return codeFile;
+			}
+			else {
+				System.out.println("Not a valid option");
+			}
+		}
+	}
+	/*---------------------|
+	 *                     |
+	 * Lexer Main Method   |
+	 *                     |
+	 ---------------------*/
 	public static void main(String[] args) {
-		String cString = ScanFileReturnCharString("test_file1.txt");
+		
+		File testFile = getTestFile();
+		String cString = ScanFileReturnCharString(testFile);
+		//System.out.println(cString);
 		List<Token> tList = lex(cString);
-		String space = " ";
-		System.out.println(Arrays.toString(tList.toArray()));
-		//System.out.println(Arrays.toString(progList.toArray()));
-		System.out.println(Arrays.toString(errorList.toArray()));
+		//printLexReport(tList);
+		//System.out.println(tList.get(0));
+		//System.out.println(tList.get(1));
+		//System.out.println(tList.get(2));
+		//System.out.println(tList.get(3));
+		//String space = " ";
+		//System.out.println(Arrays.toString(tList.toArray()));
+		////System.out.println(Arrays.toString(progList.toArray()));
+		//System.out.println(Arrays.toString(errorList.toArray()));
 		
 		
 	}
